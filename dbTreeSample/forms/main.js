782263c17552acf82c4b45d5add08a1a3abc7a0b
Tuan Nguyen <tuanway@gmail.com>
2016-09-05 16:39:33 -0500

    update tree main view

---
+++ dbTreeSample/forms/main.js
@@ -0,0 +1,26 @@
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"88388BD0-955E-4DA3-A967-1C0CEC29A3FF"}
+ */
+function onAction$showDBtree(event) {
+	forms.dbtree.show();
+}
+
+/**
+ * Perform the element default action.
+ *
+ * @param {JSEvent} event the event that triggered the action
+ *
+ * @private
+ *
+ * @properties={typeid:24,uuid:"7D96B470-80EC-4465-87A1-7664DC8D2BDA"}
+ */
+function onAction$showtreeview(event) {
+	forms.tree.show();
+}
